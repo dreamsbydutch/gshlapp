a9a9a1ffa0c42c5c73b17eab2989dbcb081ada20
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2024-02-21 12:40:34 -0500

    Update CurrentRoster.tsx

--- src/components/ui/CurrentRoster.tsx
+++ src/components/ui/CurrentRoster.tsx
@@ -99,8 +99,8 @@ export default function TeamRoster({ teamInfo, season }: { teamInfo: TeamInfoTyp
 														}`}>
 														{Math.round(+a?.Rating * 100) / 100}
 													</div>
-													<div className={`text-2xs my-1 col-span-3 rounded-xl ${contract?.ExpiryType === 'RFA' ? 'text-orange-700' : ''}`}>
-														{showSalaries && a.ContractEligible && salary > 999999 && moneyFormatter(salary)}
+													<div className={`text-2xs my-1 col-span-3 rounded-xl ${contract?.ExpiryType === 'RFA' ? 'text-orange-700' : ''} ${showSalaries && 'hidden'}`}>
+														{a.ContractEligible && salary > 999999 && moneyFormatter(salary)}
 													</div>
 												</div>
 											)
